ae6b86642d99536db946a63322d11a7cc6937107
ijgnd <fooijgnd@barijgnd>
2020-03-01 00:02:46 +0100

    filtered decks can't be nested

--- src/__init__.py
+++ src/__init__.py
@@ -2,10 +2,11 @@
 # License: AGPLv3
 # Copyright: 2020- ijgnd
 
+from pprint import pprint as pp
 
 from aqt import mw
 from aqt.qt import *
-from aqt.utils import tooltip
+from aqt.utils import showInfo, tooltip
 
 
 def gc(arg, fail=False):
@@ -49,35 +50,67 @@ def batch_create_filtered_decks():
         ## verify
         if not isinstance(sl, dict):
             tooltip('Invalid entry no {} in "mirror all decks as filtered"'.format(idx))
+            mw.progress.finish()
             return
         additional = sl.get("additional search term", None)
         if not (additional and isinstance(additional, str)):
             tooltip('Invalid entry no {} in "mirror all decks as filtered"-"additional search term"'.format(idx))
+            mw.progress.finish()
             return
         order = sl.get("selected by/order", False)
         if not (order and isinstance(order, int)):
             tooltip('Invalid entry no {} in "mirror all decks as filtered"-"selected by/order"'.format(idx))
+            mw.progress.finish()
             return
         parent = sl.get("parent deck name", False)
         if not (parent and isinstance(parent, str)):
             tooltip('Invalid entry no {} in "mirror all decks as filtered"-"parent deck name"'.format(idx))
+            mw.progress.finish()
             return
         resched = sl.get("reschedule based on answers")
         if not isinstance(resched, bool):
             tooltip('Invalid entry no {} in "mirror all decks as filtered"-"reschedule based on answers"'.format(idx))
+            mw.progress.finish()
             return            
         maxcards = sl.get("max cards", 9999)
         if not (maxcards and isinstance(maxcards, int)):
             maxcards = 9999
         steps = False # gc("steps")
         previewDelay = 0
-            
-        ## build
+
+        ## abort if parent exists
+        if mw.col.decks.byName(parent):
+            showInfo("The add-on doesn't work in the parent deck already exists. Aborting ...")
+            mw.progress.finish()
+            return
+
+        ## make sure to build most nested decks first
+        nestedlevels = {}
         for did in regDeckIds:
-            odeck = mw.col.decks.get(did)
-            name = parent.rstrip("::") + "::" + odeck['name']
-            searchtext = 'deck:"{}"'.format(odeck['name']) + " " + additional
-            CreateFilteredDeckWithoutGUI(name, searchtext, maxcards, order, resched, steps, previewDelay)
+            deck = mw.col.decks.get(did)
+            n = deck['name'].count("::")
+            nestedlevels.setdefault(n, []).append(str(did))
+
+        ## actually build them
+        for level in sorted(nestedlevels.keys(), reverse=True):
+            for did in nestedlevels[level]:
+                odeck = mw.col.decks.get(did)
+                # check for subdecks:
+                hassubdecks = False
+                for d in mw.col.decks.all(): 
+                    if d["name"].startswith(odeck['name'] + "::"):
+                        hassubdecks = True
+                        continue
+                # check if it has cards that are not in subdecks:
+                cids = mw.col.findCards(f'''deck:"{odeck['name']}" -deck:"{odeck['name']}::*"''')
+                if hassubdecks and cids:
+                    # I need to create a special subdeck for cards that are only in the parent
+                    # since a filterd deck can't have other filtered decks
+                    filtered_name = parent.rstrip("::") + "::" + odeck['name'] + "::" + "!!cards that are only in parent"
+                else:
+                    filtered_name = parent.rstrip("::") + "::" + odeck['name']
+                searchtext = 'deck:"{}"'.format(odeck['name']) + " " + additional
+                CreateFilteredDeckWithoutGUI(filtered_name, searchtext, maxcards, order, resched, steps, previewDelay)
         mw.reset()
         mw.moveToState("deckBrowser")
         mw.progress.finish()
